a98725bb7c3421baab20f1f1f08482e4d4c1d1c0
Michal Baran <michalasakura@gmail.com>
2018-08-21 00:20:06 +0100

    Removed PerActivity scope

--- app/src/main/java/io/baranmichal/thecaseagainstlivedata/movies/di/MoviesComponent.kt
+++ app/src/main/java/io/baranmichal/thecaseagainstlivedata/movies/di/MoviesComponent.kt
@@ -1,11 +1,8 @@
 package io.baranmichal.thecaseagainstlivedata.movies.di
 
 import dagger.Subcomponent
-import io.baranmichal.thecaseagainstlivedata.base.di.scopes.PerActivity
-import io.baranmichal.thecaseagainstlivedata.movies.presenter.MoviesPresenter
 import io.baranmichal.thecaseagainstlivedata.movies.view.MoviesActivity
 
-@PerActivity
 @Subcomponent(modules=[MoviesModule::class])
 interface MoviesComponent{
     fun inject(activity: MoviesActivity)
